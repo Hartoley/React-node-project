69c60361376ad7b257b05709e4cc5dd81f13c090
hartoley <tolanijimoh1@gmail.com>
2024-04-29 14:48:13 +0100

    changed password to string

--- Middleware/adminvalidator.js
+++ Middleware/adminvalidator.js
@@ -4,7 +4,7 @@ const emailregex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 const adminvalidator = yup.object().shape({
     username:yup.string().min(5, 'username must not be less than 5 characters'),
     email:yup.string().required(true, 'email is required').matches(emailregex, 'email must be valid'),
-    password:yup.number().required('password is required')
+    password:yup.string().required('password is required')
 })
 
 module.exports = {adminvalidator}
