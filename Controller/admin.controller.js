6f3280ec990f1ba989a23977e0cdf45b247db1f5
hartoley <tolanijimoh1@gmail.com>
2024-04-29 16:01:07 +0100

    added data

--- Controller/admin.controller.js
+++ Controller/admin.controller.js
@@ -73,7 +73,7 @@ const getadminlogin =(req, res) =>{
 const getData = (req, res) => {
   const data = adminmodel.find({})
   console.log(data);
-  res.send("data received")
+  res.send("data received", data)
 }
 
 module.exports = {admindash, adminsignup, getadminsignup, getadminlogin, adminlogin, getData}
