a2e1bdc448946cce0d7738ea48331ee56d2610bc
Hartoley <tolanijimoh1@gmail.com>
2025-08-03 14:02:48 +0100

    `Fixed whitespace and formatting issues in index.js`

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-const express = require ("express")
-const app = express()
-const bodyParser = require("body-parser")
-const mongoose = require('mongoose')
-const cors = require("cors")
-require('dotenv').config()
-const ejs = require('ejs')
-app.set("view engine", "ejs")
-app.use(bodyParser.json())
-app.use(cors({origin:"*"}))
-app.use(bodyParser.urlencoded({extended:true, limit:"100mb"}))
-const multer = require('multer')
-const adminrouter = require('./Routes/admin.route')
-const studentrouter = require('./Routes/student.route')
-const courserouter = require ('./Routes/courses.route')
-const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('/', adminrouter)
-app.use('/udemy', studentrouter)
-app.use('/courses', courserouter)
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
+const multer = require("multer");
+const adminrouter = require("./Routes/admin.route");
+const studentrouter = require("./Routes/student.route");
+const courserouter = require("./Routes/courses.route");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+app.use("/", adminrouter);
+app.use("/udemy", studentrouter);
+app.use("/courses", courserouter);
 
- 
+const port = process.env.PORT || 5012;
+const uri = process.env.MONGODB_URI;
 
-
-
-
-
-
-const port = process.env.PORT || 5008
-const uri = process.env.MONGODB_URI
-
-const connect = async () =>{
-    try {
-      const connected = await mongoose.connect(uri) 
-     if (connected) {
-        console.log("connected to database");
-     }
-    } catch (error) {
-       console.log(error);  
+const connect = async () => {
+  try {
+    const connected = await mongoose.connect(uri);
+    if (connected) {
+      console.log("connected to database");
     }
-}
-connect()
+  } catch (error) {
+    console.log(error);
+  }
+};
+connect();
 
-app.listen(port,()=>{
-    console.log("app started at port" + port);
- })
+app.listen(port, () => {
+  console.log("app started at port" + port);
+});
